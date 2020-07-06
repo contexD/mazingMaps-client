24c6502fbcc4b9620305106b4184a51a90192a9a
contexD <maxconte232@gmail.com>
2020-07-06 11:12:46 +0200

    created custom node with naked input field

---
+++ src/components/inputNode.js
@@ -0,0 +1,44 @@
+import React, { memo } from "react";
+import { Handle } from "react-flow-renderer";
+import InputBase from "@material-ui/core/InputBase";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  margin: {
+    margin: theme.spacing(1),
+  },
+}));
+
+export default memo(({ data }) => {
+  const classes = useStyles();
+  return (
+    <>
+      <Handle
+        type="target"
+        postion="left"
+        style={{ background: "#fff" }}
+        onConnect={(params) => console.log("handle onConnect", params)}
+      />
+      <div>
+        <InputBase
+          className={classes.margin}
+          defaultValue={data.label}
+          onChange={data.onChange}
+          inputProps={{ "aria-label": "naked" }}
+        />
+        <Handle
+          type="source"
+          position="right"
+          id="a"
+          style={{ top: 10, background: "#fff" }}
+        />
+        <Handle
+          type="source"
+          position="right"
+          id="b"
+          style={{ bottom: 10, top: "auto", background: "#fff" }}
+        />
+      </div>
+    </>
+  );
+});
