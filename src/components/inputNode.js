84de1659e8d5baaa75283a45d1dcd462b38c3549
contexD <maxconte232@gmail.com>
2020-07-13 09:44:34 +0200

    State management of InputBase value realized with Apollo client

--- src/components/inputNode.js
+++ src/components/inputNode.js
@@ -1,30 +1,52 @@
-import React, { memo } from "react";
+import React, { memo, useState } from "react";
 import { Handle } from "react-flow-renderer";
 import InputBase from "@material-ui/core/InputBase";
 import { makeStyles } from "@material-ui/core/styles";
+import { useMutation } from "@apollo/react-hooks";
+import { UPDATE_VERTEX_LABEL } from "../cache/mutations";
 
 const useStyles = makeStyles((theme) => ({
   margin: {
     margin: theme.spacing(1),
   },
+  node: {
+    background: "rgba(142,142,142,0.5)",
+    border: "2px",
+    borderRadius: "5px",
+    padding: "5%",
+    boxShadow: "2px 2px 2px rgba(160,160,160,0.8)",
+  },
 }));
 
-export default memo(({ data }) => {
+export default memo(({ id, data }) => {
   const classes = useStyles();
+  const [updateLabel] = useMutation(UPDATE_VERTEX_LABEL);
+
+  const handleChange = (event) => {
+    updateLabel({
+      variables: { id, newLabel: event.target.value },
+    });
+  };
+
   return (
     <>
-      <Handle type="target" postion="top" style={{ background: "#fff" }} />
+      <Handle
+        type="target"
+        postion="top"
+        style={{ background: "rgba(120,120,120,0.5)" }}
+      />
       <div>
         <InputBase
-          className={classes.margin}
           value={data.label}
-          onChange={data.onChange}
-          inputProps={{ "aria-label": "naked" }}
+          onChange={handleChange}
+          inputProps={{ style: { textAlign: "center" } }}
+          multiline={true}
+          className={classes.node}
         />
         <Handle
           type="source"
           position="bottom"
-          style={{ background: "#fff" }}
+          style={{ background: "rgba(120,120,120,0.5)" }}
         />
       </div>
     </>
