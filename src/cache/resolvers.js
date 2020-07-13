6a3f1ae1ec605628bbfda5c0f7192b2216d9c775
contexD <maxconte232@gmail.com>
2020-07-13 09:36:59 +0200

    Adds local resolver for updateVertexLabel

--- src/cache/resolvers.js
+++ src/cache/resolvers.js
@@ -1,1 +1,30 @@
-export const resolvers = {};
+import gql from "graphql-tag";
+
+export const resolvers = {
+  Mutation: {
+    updateVertexLabel: (
+      _root,
+      { id: vertexId, newLabel },
+      { cache, getCacheKey }
+    ) => {
+      const id = getCacheKey({ __typename: "Vertex", id: vertexId });
+      const fragment = gql`
+        fragment label on Vertex {
+          data {
+            label
+          }
+        }
+      `;
+      const res = cache.readFragment({ fragment, id });
+      const data = {
+        __typename: "Vertex",
+        data: { __typename: "Data", label: newLabel },
+      };
+      cache.writeData({
+        id,
+        data,
+      });
+      return data;
+    },
+  },
+};
