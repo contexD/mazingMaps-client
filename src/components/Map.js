14e18d02c7e96605681a0160fcd2d4eefeec1892
contexD <maxconte232@gmail.com>
2020-07-14 12:04:46 +0200

    Removes unused variables and unnecessary imports

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,21 +1,14 @@
 import React, { useState } from "react";
-import { useApolloClient, useMutation, useQuery } from "react-apollo";
+import { useMutation } from "react-apollo";
 import ReactFlow, { Background, Controls } from "react-flow-renderer";
 
 import Menu from "@material-ui/core/Menu";
 import MenuItem from "@material-ui/core/MenuItem";
 
-import DialogForm from "../components/DialogForm";
 import inputNode from "./inputNode";
 import { showMessage } from "../utils/appState";
 import { GET_GRAPH } from "../cache/queries";
 import { CREATE_VERTEX } from "../cache/mutations";
-
-// const elements = [
-//   { id: "1", data: { label: "Node 1" }, position: { x: 250, y: 5 } },
-//   { id: "2", data: { label: "Node 2" }, position: { x: 100, y: 100 } },
-//   { id: "e1-2", source: "1", target: "2", animated: true },
-// ];
 
 const graphStyles = { width: "100%", height: "93vh" };
 
@@ -25,8 +18,6 @@ const initialState = {
 };
 
 export default function Map(props) {
-  const client = useApolloClient();
-
   const [createVertex, { loading, error, data }] = useMutation(CREATE_VERTEX, {
     update(
       cache,
@@ -51,11 +42,7 @@ export default function Map(props) {
 
   const [open, setOpen] = useState(false);
   const [stateCoord, setStateCoord] = useState(initialState);
-  const [newNodeData, setNewNodeData] = useState("");
-  const [newNodeCoord, setNewNodeCoord] = useState({ x: null, y: null });
 
-  if (!loading && data) console.log("vertices data", data);
-  if (error) console.log("error", error);
   const elements = [...props.graphData.vertices, ...props.graphData.edges];
 
   /* handlers for context menu */
@@ -65,7 +52,6 @@ export default function Map(props) {
       mouseX: event.clientX - 2,
       mouseY: event.clientY - 4,
     });
-    setNewNodeCoord({ x: event.clientX - 2, y: event.clientY - 4 });
   };
 
   const handleCloseMenu = () => {
