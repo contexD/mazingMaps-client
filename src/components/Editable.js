585515487bf17b5d3e59f113e1d7ad092efc9588
contexD <maxconte232@gmail.com>
2020-07-16 10:08:01 +0200

    Creates Editable component for use in Map

---
+++ src/components/Editable.js
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import Latex from "react-latex";
+
+export default function Editable({
+  text,
+  type,
+  placeholder,
+  children,
+  ...props
+}) {
+  const [isEditing, setEditing] = useState(false);
+
+  //event handler for pressing any key while editing
+  const handleKeyDown = (event, type) => {
+    //handle on key press
+  };
+  return (
+    <section {...props}>
+      {isEditing ? (
+        <div
+          onBlur={() => setEditing(false)}
+          onKeyDown={(e) => handleKeyDown(e, type)}
+        >
+          {children}
+        </div>
+      ) : (
+        <div onDoubleClick={() => setEditing(true)}>
+          <Latex>{String.raw`${
+            text || placeholder || `Click to edit \LaTeX`
+          }`}</Latex>
+        </div>
+      )}
+    </section>
+  );
+}
