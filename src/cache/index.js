af81ac6a02bf10cae1f1ebcaa14533f069e56e36
contexD <maxconte232@gmail.com>
2020-07-21 20:21:23 +0200

    Create InMemoryCache with field policies

---
+++ src/cache/index.js
@@ -0,0 +1,31 @@
+import { makeVar } from "@apollo/client";
+import { InMemoryCache } from "apollo-cache-inmemory";
+
+//reactive variables for app state
+const appLoading = makeVar(false);
+const showToast = makeVar(false);
+const toastMessage = makeVar("");
+
+export const cache = new InMemoryCache({
+  typePolicies: {
+    appState: {
+      fields: {
+        loading: {
+          read() {
+            return appLoading();
+          },
+        },
+        showToast: {
+          read() {
+            return showToast();
+          },
+        },
+        message: {
+          read() {
+            return toastMessage();
+          },
+        },
+      },
+    },
+  },
+});
