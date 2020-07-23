8f5d138a8ffcd699cb51442b73192cd37d3cb1fb
contexD <maxconte232@gmail.com>
2020-07-23 17:21:44 +0200

    Remove unused queries

--- src/model/operations/queries.js
+++ src/model/operations/queries.js
@@ -1,34 +1,5 @@
 import gql from "graphql-tag";
 
-export const GET_APP_STATE = gql`
-  query getAppState {
-    loggedIn @client
-    appLoading @client
-    showMessage @client
-    message @client {
-      severity
-      text
-    }
-    auth @client {
-      accessToken
-      me {
-        id
-        firstName
-        lastName
-        email
-      }
-    }
-  }
-`;
-
-export const IS_LOGGED_IN = gql`
-  query {
-    getAppState {
-      loggedIn @client
-    }
-  }
-`;
-
 export const ME = gql`
   query {
     me {
@@ -36,41 +7,6 @@ export const ME = gql`
       firstName
       lastName
       email
-    }
-  }
-`;
-
-export const ME_CACHE = gql`
-  query {
-    me @client {
-      id
-      firstName
-      lastName
-      email
-    }
-  }
-`;
-
-export const IS_APP_LOADING = gql`
-  query getAppState {
-    appLoading @client
-  }
-`;
-
-export const MESSAGE = gql`
-  query getAppState {
-    showMessage @client
-    message @client {
-      severity
-      text
-    }
-  }
-`;
-
-export const GET_SELECTED_NODE = gql`
-  query {
-    selectedNode @client {
-      id
     }
   }
 `;
