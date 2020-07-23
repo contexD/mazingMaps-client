09310bed58fb4edd3e8b62f3e97983bb4cca18bb
contexD <maxconte232@gmail.com>
2020-07-23 22:10:05 +0200

    Create hook useEdge

---
+++ src/hooks/useEdge.js
@@ -0,0 +1,57 @@
+import { useMutation } from "@apollo/client";
+import { GET_GRAPH } from "../model/operations/queries";
+import { CREATE_EDGE, DELETE_EDGE } from "../model/operations/mutations";
+
+export default function useEdge() {
+  const [createEdge] = useMutation(CREATE_EDGE, {
+    update(
+      cache,
+      {
+        data: {
+          createEdge: { edge },
+        },
+      }
+    ) {
+      const data = cache.readQuery({
+        query: GET_GRAPH,
+        variables: { id: props.graphId },
+      });
+      data.graph.edges = [...data.graph.edges, edge];
+      cache.writeQuery({
+        query: GET_GRAPH,
+        variables: { id: props.graphId },
+        data,
+      });
+    },
+  });
+  //callback for react flow renderer
+  const createLink = ({ source, target }) => {
+    createEdge({ variables: { sourceId: source, targetId: target } });
+  };
+
+  const [deleteLink] = useMutation(DELETE_EDGE, {
+    update(
+      cache,
+      {
+        data: {
+          deleteEdge: { edge },
+        },
+      }
+    ) {
+      const data = cache.readQuery({
+        query: GET_GRAPH,
+        variables: { id: props.graphId },
+      });
+      data.graph.edges = [...data.graph.edges].filter(
+        (ele) => ele.id !== edge.id
+      );
+      cache.writeQuery({
+        query: GET_GRAPH,
+        variables: { id: props.graphId },
+        data,
+      });
+    },
+  });
+
+  return { createLink, deleteLink };
+}
