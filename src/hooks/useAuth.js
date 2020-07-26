20d4c70eba004e9a495a03d8bf80d7c58f2af065
contexD <maxconte232@gmail.com>
2020-07-26 12:04:47 +0200

    Set message after sendLogin mutation completed in useAuth

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -6,8 +6,9 @@ import useMessage from "../hooks/useMessage";
 export default function useAuth() {
   const { setMessage, setShowMsg } = useMessage();
 
-  const [sendLogin] = useMutation(SEND_LOGIN_DATA, {
+  const [sendLogin, { client }] = useMutation(SEND_LOGIN_DATA, {
     onCompleted: ({ signIn: { token, message, success } }) => {
+      client.resetStore();
       localStorage.setItem("token", token.jwt);
       setMessage({ text: message, severity: success });
       setShowMsg(true);
