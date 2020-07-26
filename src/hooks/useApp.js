06602b1e3fb15dee3127701a3952a497e9bcac84
contexD <maxconte232@gmail.com>
2020-07-26 12:12:40 +0200

    Rename useMessage into useApp

---
+++ src/hooks/useApp.js
@@ -0,0 +1,11 @@
+import { toastMessageVar, showToastVar, appLoadingVar } from "../model/cache";
+
+export default function useApp() {
+  const setMessage = (text, severity) => toastMessageVar({ text, severity });
+
+  const setShowMsg = (show) => showToastVar(show);
+
+  const setAppLoading = (loading) => appLoadingVar(loading);
+
+  return { setMessage, setShowMsg };
+}
--- src/hooks/useMessage.js
+++
@@ -1,9 +0,0 @@
-import { toastMessageVar, showToastVar } from "../model/cache";
-
-export default function useMessage() {
-  const setMessage = (text, severity) => toastMessageVar({ text, severity });
-
-  const setShowMsg = (show) => showToastVar(show);
-
-  return { setMessage, setShowMsg };
-}
