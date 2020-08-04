1fe7f6e968d2fd0380a45d6e2176c57531f44476
contexD <maxconte232@gmail.com>
2020-08-04 18:01:01 +0200

    Add setIsLoggedIn method

--- src/hooks/useApp.js
+++ src/hooks/useApp.js
@@ -1,14 +1,21 @@
-import { toastMessageVar, showToastVar, appLoadingVar } from "../model/cache";
+import {
+  toastMessageVar,
+  showToastVar,
+  appLoadingVar,
+  isLoggedInVar,
+} from "../model/cache";
 
 export default function useApp() {
   const setMessage = (text, success) => {
     const severity = success ? "success" : "error";
     toastMessageVar({ text, severity });
-  }
+  };
 
   const setShowMsg = (show) => showToastVar(show);
 
+  const setIsLoggedIn = (loggedIn) => isLoggedInVar(loggedIn);
+
   const setAppLoading = (loading) => appLoadingVar(loading);
 
-  return { setMessage, setShowMsg, setAppLoading };
+  return { setMessage, setShowMsg, setIsLoggedIn, setAppLoading };
 }
