984facccb0e531fe168e789aef2c3f587e953400
contexD <maxconte232@gmail.com>
2020-08-05 11:38:23 +0200

    Add svg and welcome message

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,49 @@
 import React from "react";
-import Latex from "react-latex";
+import {Link as RouterLink} from "react-router-dom";
 
-import { Typography } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import { Grid, Typography, Button } from "@material-ui/core";
+
+import { ReactComponent as MindMapSvg } from "./media/undraw_mind_map.svg";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    spacing: theme.spacing(0),
+    direction: "column",
+    alignItems: "center",
+    minHeight: "100vh",
+  },
+  descr: {
+    marginTop: "5%",
+  },
+}));
 
 export default function Home() {
+  const classes = useStyles();
   return (
-    <div>
-      <Typography variant="h1">Home</Typography>
-      <Latex>
-        {String.raw`This is a test $\sum_{i = 1}^{n} i = \dfrac{n(n+1)}{2}$`}
-      </Latex>
-    </div>
+    <Grid container className={classes.root}>
+      <Grid container justify="center">
+        <Grid item xs={4}>
+          <Typography variant="h2">MazingMapper</Typography>
+          <Typography variant="h3">Create mind maps online</Typography>
+          <Typography variant="h4" className={classes.descr}>
+            It's never been easier and more intuitive to organize your ideas.
+          </Typography>
+          <Button
+            variant="contained"
+            color="primary"
+            size="large"
+            component={RouterLink}
+            to="/signup"
+            className={classes.descr}
+          >
+            Get started
+          </Button>
+        </Grid>
+        <Grid item xs={6}>
+          <MindMapSvg />
+        </Grid>
+      </Grid>
+    </Grid>
   );
 }
