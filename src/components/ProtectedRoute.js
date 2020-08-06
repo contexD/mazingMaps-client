f3f1c797d30a6befe1b95826b08e7664ab8d931b
contexD <maxconte232@gmail.com>
2020-08-06 16:20:36 +0200

    Create ProtectedRoute component

---
+++ src/components/ProtectedRoute.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Redirect } from "react-router-dom";
+
+import { useQuery } from "@apollo/client";
+import { IS_LOGGED_IN } from "../model/operations/queries";
+
+export default function ProtectedRoute(props) {
+  const { data } = useQuery(IS_LOGGED_IN);
+  const { Component } = props;
+
+  return data && data.isLoggedIn ? (
+    <Component />
+  ) : (
+    <Redirect to={{ pathname: "/" }} />
+  );
+}
